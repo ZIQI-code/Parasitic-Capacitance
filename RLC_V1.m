b647833b9ca8118b7fd7192e02a189cdefb4c04b
zq001 <zq001@DESKTOP-IALB3JK>
2020-11-13 18:55:32 +0800

    update 11.13

--- RLC_V1.m
+++ RLC_V1.m
@@ -1,2 +1,33 @@
 %%Circuit equation of the simple RLC Circuit.
-function 
+function Zcal = z_4_element_Real(x, f0)
+% CIRCUIT:
+% |--R---L-----|       1/(L+R)    1./(1j*w0*x(2)*u2+x(1))
+% |              |
+% |------Cparasitic------|         Cp    1j*w0*x(3)*u1
+% |              |
+% |-------Cspacing----|             Cs   1j*w0*x(4)*u1
+%
+% parameter meaning:
+% a(1)--->R
+% a(2)--->L
+% a(3)--->Cp
+% a(4)--->Cs
+% 
+% F = Zeq = 1/((1/R+jwL) + jwCp + jwCs)
+w0 = 2*pi*f0;
+u1 = 1;
+u2 = 1;
+Zcal = real(1./( (1./(1j*w0*x(2)*u2+x(1))) + 1j*w0*x(3)*u1 + 1j*w0*x(4)*u1 ));
+
+end
+
+
+
+function Zcal = z_4_element_Imag(x, f0)
+
+w0 = 2*pi*f0;
+u1 = 1;
+u2 = 1;
+Zcal = imag(1./( (1./(1j*w0*x(2)*u2+x(1))) + 1j*w0*x(3)*u1 + 1j*w0*x(4)*u1 ));
+
+end
--- state_1_10_1.m
+++ state_1_10_1.m
@@ -58,27 +58,27 @@ L2I_N18K_1_1 = csvread('L2N18_01_KNITCONNECTION_LENGTHINTERFERENCE_01_9_28.CSV',
 L2R_N18K_2_1 = csvread('L2N18_02_KNITCONNECTION_LENGTHINTERFERENCE_01_9_28.CSV',3,1,[3,1,201,1]);
 L2I_N18K_2_1 = csvread('L2N18_02_KNITCONNECTION_LENGTHINTERFERENCE_01_9_28.CSV',3,2,[3,2,201,2]);
 
-figure(3)
-plot(L1F_N18C_0_1,L1R_N18C_0_1,L1F_N18C_0_1,L1R_N18C_1_1,L1F_N18C_0_1,L1R_N18C_2_1)
-hold on 
-plot(L2F_N18K_0_1,L2R_N18K_0_1,L2F_N18K_0_1,L2R_N18K_1_1,L2F_N18K_0_1,L2R_N18K_2_1)
-legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
-title('Copper Wire and Knitted Wire N18 Real')
-xlabel('Frequency (Hz)')
-ylabel('Ohms')
-grid on
-hold off
-
-figure(4)
-plot(L1F_N18C_0_1,L1I_N18C_0_1,L1F_N18C_0_1,L1I_N18C_1_1,L1F_N18C_0_1,L1I_N18C_2_1)
-hold on 
-plot(L2F_N18K_0_1,L2I_N18K_0_1,L2F_N18K_0_1,L2I_N18K_1_1,L2F_N18K_0_1,L2I_N18K_2_1)
-legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
-title('Copper Wire and Knitted Wire N18 Imaginary')
-xlabel('Frequency (Hz)')
-ylabel('Reactance')
-grid on
-hold off
+% figure(3)
+% plot(L1F_N18C_0_1,L1R_N18C_0_1,L1F_N18C_0_1,L1R_N18C_1_1,L1F_N18C_0_1,L1R_N18C_2_1)
+% hold on 
+% plot(L2F_N18K_0_1,L2R_N18K_0_1,L2F_N18K_0_1,L2R_N18K_1_1,L2F_N18K_0_1,L2R_N18K_2_1)
+% legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
+% title('Copper Wire and Knitted Wire N18 Real')
+% xlabel('Frequency (Hz)')
+% ylabel('Ohms')
+% grid on
+% hold off
+% 
+% figure(4)
+% plot(L1F_N18C_0_1,L1I_N18C_0_1,L1F_N18C_0_1,L1I_N18C_1_1,L1F_N18C_0_1,L1I_N18C_2_1)
+% hold on 
+% plot(L2F_N18K_0_1,L2I_N18K_0_1,L2F_N18K_0_1,L2I_N18K_1_1,L2F_N18K_0_1,L2I_N18K_2_1)
+% legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
+% title('Copper Wire and Knitted Wire N18 Imaginary')
+% xlabel('Frequency (Hz)')
+% ylabel('Reactance')
+% grid on
+% hold off
 
 %% Study of N18 knitted wire and copper wire with the same diameter
 L0F_N18N_0_2 = csvread('L0N18_00_0.43KNIT_01_10_1.CSV',3,0,[3,0,201,0]);
@@ -96,7 +96,7 @@ plot(L0F_N18N_0_2,L0R_N18N_0_2,L0F_N18N_0_2,L0R_N18N_1_2,L0F_N18N_0_2,L0R_N18N_2
 legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
 title('Copper Wire and Knitted Wire N18 Real')
 xlabel('Frequency (Hz)')
-ylabel('Ohms')((pi*0.22*0.22)*(18*18)*4*pi*10^-7)/19.8
+ylabel('Ohms')
 grid on
 hold off
 
@@ -104,3 +104,49 @@ figure(6)
 plot(L1F_N18C_0_1,L1I_N18C_0_1,L1F_N18C_0_1,L1I_N18C_1_1,L1F_N18C_0_1,L1I_N18C_2_1)
 hold on
 plot(L0F_N18N_0_2,L0I_N18N_0_2,L0F_N18N_0_2,L0I_N18N_1_2,L0F_N18N_0_2,L0I_N18N_2_2)
+legend('N18C-0','N18C-1','N18C-2','N18K-0','N18K-1','N18K-2')
+title('Copper Wire and Knitted Wire N18 Imaginary')
+xlabel('Frequency (Hz)')
+ylabel('Reactance')
+grid on
+hold off
+%% Study of golden Knitted Wire
+L0F_N18GN_0 = csvread('L0N18_00_GKNIT_01_10_8.CSV',3,0,[3,0,201,0]);
+L0R_N18GN_0 = csvread('L0N18_00_GKNIT_01_10_8.CSV',3,1,[3,1,201,1]);
+L0I_N18GN_0 = csvread('L0N18_00_GKNIT_01_10_8.CSV',3,2,[3,2,201,2]);
+L0R_N18GN_1 = csvread('L0N18_01_GKNIT_01_10_8.CSV',3,1,[3,1,201,1]);
+L0I_N18GN_1 = csvread('L0N18_01_GKNIT_01_10_8.CSV',3,2,[3,2,201,2]);
+L0R_N18GN_2 = csvread('L0N18_02_GKNIT_01_10_8.CSV',3,1,[3,1,201,1]);
+L0I_N18GN_2 = csvread('L0N18_02_GKNIT_01_10_8.CSV',3,2,[3,2,201,2]);
+
+figure(7)
+plot(L0F_N18GN_0,L0R_N18GN_0,L0F_N18GN_0,L0R_N18GN_1,L0F_N18GN_0,L0R_N18GN_2)
+hold on
+legend('N18GN_0','N18GN_1','N18GN_2')
+title('Golden Knitted Wire N18 Real')
+xlabel('Frequency (Hz)')
+ylabel('Ohms')
+grid on
+hold off
+
+figure(8)
+plot(L0F_N18GN_0,L0I_N18GN_0,L0F_N18GN_0,L0I_N18GN_1,L0F_N18GN_0,L0I_N18GN_2)
+hold on
+legend('N18GN_0','N18GN_1','N18GN_2')
+title('Golden Knitted Wire Imaginary')
+xlabel('Frequency (Hz)')
+ylabel('Reactance')
+grid on
+hold off
+% 
+%% 
+R = 36.82;
+L = 4.96e-6;
+S = 2.415e-12;
+w = 4.32e7 * 2* pi;
+solve((L^2 *S*w^2 * (x^2 * R^2 * w^2 + 1))-L*(x^2 * R^2 * w^2 + 2*x*R^2 *S*w^2 + 1)+R^2 *(x+S)==0,x)
+x
+
+
+
+
