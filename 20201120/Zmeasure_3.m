80912389b8857e7903440b1b6f2ed716d5c5b3b6
ZIQI-code <zq001@bucknell.edu>
2020-11-28 21:10:05 +0800

    11.28 simulation

--- 20201120/Zmeasure_3.m
+++ 20201120/Zmeasure_3.m
@@ -1,36 +1,54 @@
-clear
+%% read data
+clear 
 
-data_real = csvread('L_N10_LE_00_HF.CSV',3,1,[3,1,201,1]);
-data_imag = csvread('L_N10_LE_00_HF.CSV',3,2,[3,2,201,2]);
-freq = csvread('L_N10_LE_00_HF.CSV', 3, 0, [3, 0, 201, 0]);
+K_F_N18_00 = csvread('S_N18_SE_00_HF.CSV',3,0,[3,0,201,0]);
+K_R_N18_00 = csvread('S_N18_SE_00_HF.CSV',3,1,[3,1,201,1]);
+K_I_N18_00 = csvread('S_N18_SE_00_HF.CSV',3,2,[3,2,201,2]);
+K_R_N18_01 = csvread('S_N18_SE_01_HF.CSV',3,1,[3,1,201,1]);
+K_I_N18_01 = csvread('S_N18_SE_01_HF.CSV',3,2,[3,2,201,2]);
+K_R_N18_02 = csvread('S_N18_SE_02_HF.CSV',3,1,[3,1,201,1]);
+K_I_N18_02 = csvread('S_N18_SE_02_HF.CSV',3,2,[3,2,201,2]);
+
+K_R_N18 = zeros(199,1);
+K_I_N18 = zeros(199,1);
+
+for i = 1:199
+    K_R_N18(i) = (K_R_N18_00(i)+K_R_N18_01(i)+K_R_N18_02(i))/3;
+    K_I_N18(i) = (K_I_N18_00(i)+K_I_N18_01(i)+K_I_N18_02(i))/3;
+end 
+%% 
+
+data_real = K_R_N18;
+data_imag = K_I_N18;
+freq = K_F_N18_00;
 
 fcn = @(c) Zmeasure_Objective_3(c, freq, data_real, data_imag);
 
-p = sdo.getParameterFromModel('RC_Circuit_3', {'Cs','Cp','R'});
+p = sdo.getParameterFromModel('RC_Circuit_3', {'Cs','Cp'});
 
 p(1).Value = 1e-15;
 p(1).Minimum = 0;
-p(1).Maximum = 1e-6;
+p(1).Maximum = 1e-3;
 p(2).Value = 1e-15;
 p(2).Minimum = 0;
-p(2).Maximum = 1e-9;
+p(2).Maximum = 1e-3;
 % p(3).Value = 1e-14;
 % p(3).Minimum = 0;
-p(3).Value = 10;
-p(3).Minimum = 0;
-p(3).Maximum = 200;
+% p(3).Value = 10;
+% p(3).Minimum = 0;
+% p(3).Maximum = 200;
 
 opt_result = sdo.optimize(fcn, p)
 
 sdo.setValueInModel('RC_Circuit_3', 'Cs', opt_result(1).Value);
 sdo.setValueInModel('RC_Circuit_3', 'Cp', opt_result(2).Value);
-sdo.setValueInModel('RC_Circuit_3', 'R', opt_result(3).Value);
+% sdo.setValueInModel('RC_Circuit_3', 'R', opt_result(3).Value);
 %% 
-clear
 
-data_real = csvread('L_N10_LE_00_HF.CSV',3,1,[3,1,201,1]);
-data_imag = csvread('L_N10_LE_00_HF.CSV',3,2,[3,2,201,2]);
-freq = csvread('L_N10_LE_00_HF.CSV', 3, 0, [3, 0, 201, 0]);
+
+% data_real = csvread('L_N10_LE_00_HF.CSV',3,1,[3,1,201,1]);
+% data_imag = csvread('L_N10_LE_00_HF.CSV',3,2,[3,2,201,2]);
+% freq = csvread('L_N10_LE_00_HF.CSV', 3, 0, [3, 0, 201, 0]);
 z_data = power_zmeter('RC_Circuit_3', freq');
 figure(9)
 plot(freq, imag(z_data.Z), freq, data_imag)
--- 20201120/Zmeasure_Objective_3.m
+++ 20201120/Zmeasure_Objective_3.m
@@ -1,12 +1,12 @@
 function vals = Zmeasure_Objective_3(p, freq, data_real, data_imag)
     sdo.setValueInModel('RC_Circuit_3', 'Cs', p(1).Value);
     sdo.setValueInModel('RC_Circuit_3', 'Cp', p(2).Value);
-    sdo.setValueInModel('RC_Circuit_3', 'R', p(3).Value);
+%     sdo.setValueInModel('RC_Circuit_3', 'R', p(3).Value);
 
     z_data = power_zmeter('RC_Circuit_3', freq');
 
-    low = 7;
-    high = 140;
+    low = 1;
+    high = 60;
     N = (high - low + 1);
     error = sum(abs(real(z_data.Z(low:high)) - data_real(low:high)) .^ 2 + ...
     abs(imag(z_data.Z(low:high)) - data_imag(low:high)) .^ 2)./N;
