7e1b405a1362ce6da2fad9ae3fa1b76496f2f883
Leo Wang <yw7uc@virginia.edu>
2021-01-15 06:50:08 +0800

    eculidean distance

---
+++ 20201120/distance.py
@@ -0,0 +1,30 @@
+import math
+
+count = 4
+
+infile = open('point.txt', 'r')
+outfile = open('distance.txt', 'r+')
+outfile.truncate()
+outfile.seek(0)
+
+for i in range(count):
+	name = infile.readline()
+	infile.readline()
+	exp = infile.readline()
+	infile.readline()
+	sim = infile.readline()
+	infile.readline()
+
+	exp = exp.split()
+	sim = sim.split()
+
+	distance = 0
+	for j in range(6):
+		exp[j] = float(exp[j])
+		sim[j] = float(sim[j])
+		distance += math.pow(exp[j] - sim[j], 2)
+
+	outfile.write('{}: {} e-11\n'.format(name[:-1], math.sqrt(distance)))
+
+infile.close()
+outfile.close()
